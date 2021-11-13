085d3a92f7a0c96d8d0f7743dbd572f9792d6e2f
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:38:12 +0000

    11 ran tests, no prediction, got green

--- files/gol.test.ts
+++ files/gol.test.ts
@@ -1,9 +1,9 @@
 import { isCellAlive } from './gol';
 
 describe('isCellAlive', () => {
-  //it('should be die when has no neighbor', () => {
-  //  expect(isCellAlive()).toEqual(false);
-  //});
+  it('should be die when has no neighbor', () => {
+    expect(isCellAlive(0)).toEqual(false);
+  });
   
   it('should be alive when has two neighbors', () => {
     expect(isCellAlive(2)).toEqual(true);
