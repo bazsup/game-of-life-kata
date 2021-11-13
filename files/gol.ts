6630ca1831277e36dc3e4e26fa27dc5e03296dfe
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:44:07 +0000

    19 ran tests, no prediction, got green

--- files/gol.ts
+++ files/gol.ts
@@ -1,5 +1,7 @@
 export function isCellAlive(numOfNeighbors: number, isAlive: boolean): boolean {
   const isTwoOrThreeNeighbors = numOfNeighbors === 2 || numOfNeighbors === 3
+  
+  if (!isAlive) return false
   if (isTwoOrThreeNeighbors) return true
   
   return false;
