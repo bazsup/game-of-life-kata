857127d1bd6631030dc713b676ecf557ba271383
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:35:37 +0000

    7 ran tests, no prediction, got amber

--- files/gol.test.ts
+++ files/gol.test.ts
@@ -1,9 +1,9 @@
 import { isCellAlive } from './gol';
 
 describe('isCellAlive', () => {
-  it.skip('should be die when has no neighbor', () => {
-    expect(isCellAlive()).toEqual(false);
-  });
+  //it('should be die when has no neighbor', () => {
+  //  expect(isCellAlive()).toEqual(false);
+  //});
   
   it('should be alive when has two neighbors', () => {
     expect(isCellAlive(2)).toEqual(true);
