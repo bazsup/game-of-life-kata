42375709d736c1c69deed3dcf3c8da39315cd3c8
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 14:24:58 +0000

    57 ran tests, no prediction, got green

--- files/gol.ts
+++ files/gol.ts
@@ -15,13 +15,9 @@ export interface Position {
 
 export function getNumOfNeighbors(board: string[][], position: Position): number {
   const { row, col } = position
-  const hasRightNeighbor = board[row][col + 1] == '*'
-  const hasLeftNeighbor = board[row][col - 1] == '*'
+  const hasRightNeighbor = board[row][col + 1] === '*'
+  const hasLeftNeighbor = board[row][col - 1] === '*'
   if (hasLeftNeighbor) return 1
-//  if (board[0][1] === '*' && board[0].length == 3) return 1
-//  if (board[0][0] === '*' && board[0].length == 2) return 1
-  if (hasRightNeighbor) {
-    return 1
-  }
+  if (hasRightNeighbor) return 1
   return 0;
 }
