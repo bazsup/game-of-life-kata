f5a738ad1e65f77f9cfd59aec4b990b957e31a03
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:58:56 +0000

    32 ran tests, predicted red, got amber

--- files/gol.test.ts
+++ files/gol.test.ts
@@ -1,4 +1,4 @@
-import { isCellAlive } from './gol';
+import { isCellAlive, getNumOfNeighbors } from './gol';
 
 describe('isCellAlive', () => {
   it('an alive cell should be die when has no neighbor', () => {
