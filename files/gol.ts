402646151bfea902b1b3e0c318dca0827bc8778c
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:48:52 +0000

    25 ran tests, no prediction, got green

--- files/gol.ts
+++ files/gol.ts
@@ -1,16 +1,8 @@
 export function isCellAlive(numOfNeighbors: number, isAlive: boolean): boolean {
   const isTwoOrThreeNeighbors = numOfNeighbors === 2 || numOfNeighbors === 3
   
-  if (!isAlive && numOfNeighbors == 3) return false
+  if (!isAlive && numOfNeighbors == 3) return true
   if (isAlive && isTwoOrThreeNeighbors) return true
   
   return false;
 }
-
-/*
-
-    ✓ should be die when has no neighbor (3 ms)
-    ✕ should be alive when has 2 neighbors (2 ms)
-    ✕ a dead cell should be alive when has 3 neighbors (1 ms)
-    ✓ a dead cell should still die when has 2 neighbors
-*/
