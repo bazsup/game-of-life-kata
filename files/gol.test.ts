23cf74211828250d5d7673d20045559c5859d41c
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:42:46 +0000

    18 ran tests, no prediction, got red

--- files/gol.test.ts
+++ files/gol.test.ts
@@ -1,7 +1,7 @@
 import { isCellAlive } from './gol';
 
 describe('isCellAlive', () => {
-  it('should be die when has no neighbor', () => {
+  /*it('should be die when has no neighbor', () => {
     expect(isCellAlive(0)).toEqual(false);
   });
   
@@ -11,7 +11,7 @@ describe('isCellAlive', () => {
   
   it('should be die when has 3 neighbors', () => {
     expect(isCellAlive(3)).toEqual(true);
-  });
+  });*/
   
   it('a dead cell should still die when has 2 neighbors', () => {
     const isAlive = false
