634c59a64947704a834c43b831f2afdb265228c0
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:39:30 +0000

    13 ran tests, no prediction, got green

--- files/gol.ts
+++ files/gol.ts
@@ -1,4 +1,4 @@
 export function isCellAlive(numOfNeighbors: number): boolean {
-  if (numOfNeighbors === 2) return true
+  if (numOfNeighbors === 2 || numOfNeighbors === 3) return true
   return false;
 }
