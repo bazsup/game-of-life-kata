776026d20c4a8f823e58e1e1e86843638e35de48
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 13:41:02 +0000

    15 ran tests, no prediction, got red

--- files/gol.test.ts
+++ files/gol.test.ts
@@ -12,4 +12,8 @@ describe('isCellAlive', () => {
   it('should be die when has 3 neighbors', () => {
     expect(isCellAlive(3)).toEqual(true);
   });
+  
+  it('a dead cell should still die when has 2 neighbors', () => {
+    expect(isCellAlive(2)).toEqual(false);
+  });
 });
