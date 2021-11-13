6fbbe33dc0960d8ec6ae48d596facc603021c846
4l7Y7g <4l7Y7g@cyber-dojo.org>
2021-11-13 14:28:48 +0000

    61 ran tests, no prediction, got red

--- files/gol.test.ts
+++ files/gol.test.ts
@@ -91,5 +91,18 @@ describe('getNumOfNeighbors', () => {
     
     expect(result).toEqual(1)
   });
+  
+  it('position 1, 0 should return 1 when has 1 neighbor below', () => {
+    const board = [
+      [''],
+      ['*'],
+      ['*'],
+    ];
+    const position: Position = {row: 1, col: 0}
+
+    const result = getNumOfNeighbors(board, position)
+    
+    expect(result).toEqual(1)
+  });
 
 })
